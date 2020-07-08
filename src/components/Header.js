9ed698605195573715dda1da5da7f5bfd32b514e
chanders1078 <shekharsaa@gmail.com>
2020-07-08 16:41:15 +0530

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,22 @@ export class Header extends Component {
     render() {
         return (
             <div>
-                <div className="container-fluid">
-                    <nav className="navbar navbar-expand-lg bg-light">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="navbar-toggler-icon"></span>
-                        </button>
+                <div className="container main-top">
+                    <nav className="navbar navbar-default navbar-expand-lg navbar-fixed">
+                        <div>
+                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                         </button>
+                        </div>
+                        <a className="navbar-brand m-0" href="#">Navbar</a>
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item"><a className="nav-link">Home</a></li>
-                            <li className="nav-item"><a className="nav-link">Home</a></li>
-                            <li className="nav-item"><a className="nav-link">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Portfolio</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Services</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Education</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#">Skill</a></li>
                         </ul>
                     </nav>
                 </div>
---
+++ src/components/Welcomescreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../App.css';
+
+export class Welcomescreen extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Welcomescreen;
