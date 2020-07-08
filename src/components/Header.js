e39a6b83c71b148437ed90dcded97a8a22df8d13
chanders1078 <shekharsaa@gmail.com>
2020-07-08 21:47:55 +0530

    logoset

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './../App.css';
 import Welcomescreen from './Welcomescreen';
+import logo from './images/logo.png';
 
 export class Header extends Component {
     render() {
@@ -8,7 +9,7 @@ export class Header extends Component {
             <div>
                 <div className="container main-top">
                     <nav className="navbar navbar-default navbar-expand-lg  boostnav">
-                    <a className="navbar-brand m-0" href="#">Logo</a>
+                    <a className="navbar-brand m-0" href="#"><img src={logo} className="brand-logo" alt="azaadtech"></img></a>
                         <div>
                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
                          <i class="fas fa-bars toggle-icon"></i>
